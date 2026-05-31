caf693d2f535acbd1f7a47153710f148f036dedf
Rahmat-Ullah-Khan <mdrahmatullahkhanshakib@gmail.com>
2026-05-31 10:53:54 +0600

    practice again

--- lib/app.dart
+++ lib/app.dart
@@ -6,102 +6,118 @@ import 'package:daily_practice/form_validation.dart';
 import 'package:daily_practice/home_page.dart';
 import 'package:daily_practice/own_widget.dart';
 import 'package:daily_practice/responsiveness.dart';
+import 'package:daily_practice/responsiveness_package.dart';
 import 'package:daily_practice/stackW.dart';
 import 'package:daily_practice/stackWAdv.dart';
 import 'package:daily_practice/theme_test.dart';
 import 'package:daily_practice/themes/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.lightTheme, // this is used to set the light theme of the app. it is a static property of the AppTheme class that contains the light theme of the app. it is passed as a parameter to the MaterialApp constructor.
-      darkTheme: AppTheme.darkTheme, // this is used to set the dark theme of
-      // theme: ThemeData(
-      //   //brightness: Brightness.light, // this is used to change the brightness of the app. it is used to set the color scheme of the app. it is passed as a parameter to the ThemeData constructor.
-      //   primaryColor: Colors.blue, // this is used to change the primary color of the app which is used in the app bar and other widgets that use the primary color, primaryColor is a Color which is used to set the color of the app bar and other widgets that use the primary color. it is passed as a parameter to the ThemeData constructor.
-      //   primarySwatch: Colors.blue, // this is used to change the primary swatch of the app which is used in the app bar and other widgets that use the primary swatch color, primarySwatch is a MaterialColor which is a map of colors that are used in the app. it is used to set the color of the app bar and other widgets that use the primary swatch color. it is passed as a parameter to the ThemeData constructor.
-      //   //scaffoldBackgroundColor: Colors.grey[200], // this is used to change the background color of the scaffold
-      //   elevatedButtonTheme: ElevatedButtonThemeData(
-      //     style: ElevatedButton.styleFrom(
-      //       backgroundColor: Colors.blue, // this is used to change the background color of the button
-      //       foregroundColor: Colors.white, // this is used to change the text color of the button
-      //       shape: RoundedRectangleBorder( // this is used to change the shape of the button
-      //         borderRadius: BorderRadius.circular(8), // this is used to make the button circular
-      //       ),
-      //     ),
-      //   ),
-      //   inputDecorationTheme: InputDecorationTheme(
-      //     border: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular
-      //     ),
-      //     focusedBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular when it is focused
-      //       borderSide: BorderSide(color: Colors.blue), // this is used to change the color of the border when it is focused
-      //     ),
-      //     hintStyle: TextStyle(
-      //       color: Colors.grey, // this is used to change the color of the hint text
-      //     ),
-      //   ),
-      //   textTheme: TextTheme(
-      //     headlineLarge: TextStyle(
-      //       //color: Colors.black, // this is used to change the color of the headline text
-      //       fontSize: 100, // this is used to change the font size of the headline text
-      //       fontWeight: FontWeight.bold, // this is used to change the font weight of the headline text
-      //     ),
-      //   ),
-      // ),// this is used to set the theme of the app. ThemeData is a class that contains all the properties of the theme. it is used to set the colors, fonts, and other properties of the theme. it is passed as a parameter to the MaterialApp constructor.
-      // darkTheme: ThemeData.dark().copyWith(
-      //   primaryColor: Colors.teal, // this is used to change the primary color of the dark theme
-      //   elevatedButtonTheme: ElevatedButtonThemeData(
-      //     style: ElevatedButton.styleFrom(
-      //       backgroundColor: Colors.teal, // this is used to change the background color of the button
-      //       foregroundColor: Colors.white, // this is used to change the text color of the button
-      //       shape: RoundedRectangleBorder( // this is used to change the shape of the button
-      //         borderRadius: BorderRadius.circular(8), // this is used to make the button circular
-      //       ),
-      //     ),
-      //   ),
-      //   inputDecorationTheme: InputDecorationTheme(
-      //     border: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular
-      //     ),
-      //     focusedBorder: OutlineInputBorder(
-      //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular when it is focused
-      //       borderSide: BorderSide(color: Colors.teal), // this is used to change the color of the border when it is focused
-      //     ),
-      //     hintStyle: TextStyle(
-      //       color: Colors.grey, // this is used to change the color of the hint text
-      //     ),
-      //   ),
-      //   textTheme: TextTheme(
-      //     headlineLarge: TextStyle(
-      //       //color: Colors.black, // this is used to change the color of the headline text
-      //       fontSize: 100, // this is used to change the font size of the headline text
-      //       fontWeight: FontWeight.bold, // this is used to change the font weight of the headline text
-      //     ),
-      //   ),
-      // ),// this is used to set the dark theme of the app. ThemeData.dark() is a predefined dark theme that contains all the properties of the dark theme. it is used to set the colors, fonts, and other properties of the dark theme. it is passed as a parameter to the MaterialApp constructor.
-      themeMode: ThemeMode.light, // this is used to set the theme mode of the app. ThemeMode is an enum that contains three values: light, dark, and system. it is used to set the theme mode of the app. it is passed as a parameter to the MaterialApp constructor.
-      //home: ThemeTest(),
-      initialRoute: '/responsiveness', // this is used to set the initial route of the app. it is a string that contains the name of the route. it is passed as a parameter to the MaterialApp constructor.
-      routes: { // this is used to set the routes of the app. it is a map that contains the route name as the key and the widget as the value. it is passed as a parameter to the MaterialApp constructor.
-        // '/form': (context) => FormPage(),
-        '/validation': (context) => FormValidation(),
-        //'/grid': (context) => GridV(),
-        '/theme': (context) => ThemeTest(),
-        '/home': (context) => HomePage(),
-        '/dashboard': (context) => Dashboard(),
-        '/list': (context) => Flist(),
-        '/stackw': (context) => StackW(),
-        '/stackwadv': (context) => Stackwadv(),
-        '/ownwidget': (context) => OwnWidget(),
-        '/responsiveness': (context) => Responsiveness(),
-      }
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          theme: AppTheme
+              .lightTheme, // this is used to set the light theme of the app. it is a static property of the AppTheme class that contains the light theme of the app. it is passed as a parameter to the MaterialApp constructor.
+          darkTheme:
+              AppTheme.darkTheme, // this is used to set the dark theme of
+          // theme: ThemeData(
+          //   //brightness: Brightness.light, // this is used to change the brightness of the app. it is used to set the color scheme of the app. it is passed as a parameter to the ThemeData constructor.
+          //   primaryColor: Colors.blue, // this is used to change the primary color of the app which is used in the app bar and other widgets that use the primary color, primaryColor is a Color which is used to set the color of the app bar and other widgets that use the primary color. it is passed as a parameter to the ThemeData constructor.
+          //   primarySwatch: Colors.blue, // this is used to change the primary swatch of the app which is used in the app bar and other widgets that use the primary swatch color, primarySwatch is a MaterialColor which is a map of colors that are used in the app. it is used to set the color of the app bar and other widgets that use the primary swatch color. it is passed as a parameter to the ThemeData constructor.
+          //   //scaffoldBackgroundColor: Colors.grey[200], // this is used to change the background color of the scaffold
+          //   elevatedButtonTheme: ElevatedButtonThemeData(
+          //     style: ElevatedButton.styleFrom(
+          //       backgroundColor: Colors.blue, // this is used to change the background color of the button
+          //       foregroundColor: Colors.white, // this is used to change the text color of the button
+          //       shape: RoundedRectangleBorder( // this is used to change the shape of the button
+          //         borderRadius: BorderRadius.circular(8), // this is used to make the button circular
+          //       ),
+          //     ),
+          //   ),
+          //   inputDecorationTheme: InputDecorationTheme(
+          //     border: OutlineInputBorder(
+          //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular
+          //     ),
+          //     focusedBorder: OutlineInputBorder(
+          //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular when it is focused
+          //       borderSide: BorderSide(color: Colors.blue), // this is used to change the color of the border when it is focused
+          //     ),
+          //     hintStyle: TextStyle(
+          //       color: Colors.grey, // this is used to change the color of the hint text
+          //     ),
+          //   ),
+          //   textTheme: TextTheme(
+          //     headlineLarge: TextStyle(
+          //       //color: Colors.black, // this is used to change the color of the headline text
+          //       fontSize: 100, // this is used to change the font size of the headline text
+          //       fontWeight: FontWeight.bold, // this is used to change the font weight of the headline text
+          //     ),
+          //   ),
+          // ),// this is used to set the theme of the app. ThemeData is a class that contains all the properties of the theme. it is used to set the colors, fonts, and other properties of the theme. it is passed as a parameter to the MaterialApp constructor.
+          // darkTheme: ThemeData.dark().copyWith(
+          //   primaryColor: Colors.teal, // this is used to change the primary color of the dark theme
+          //   elevatedButtonTheme: ElevatedButtonThemeData(
+          //     style: ElevatedButton.styleFrom(
+          //       backgroundColor: Colors.teal, // this is used to change the background color of the button
+          //       foregroundColor: Colors.white, // this is used to change the text color of the button
+          //       shape: RoundedRectangleBorder( // this is used to change the shape of the button
+          //         borderRadius: BorderRadius.circular(8), // this is used to make the button circular
+          //       ),
+          //     ),
+          //   ),
+          //   inputDecorationTheme: InputDecorationTheme(
+          //     border: OutlineInputBorder(
+          //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular
+          //     ),
+          //     focusedBorder: OutlineInputBorder(
+          //       borderRadius: BorderRadius.circular(8), // this is used to make the text field circular when it is focused
+          //       borderSide: BorderSide(color: Colors.teal), // this is used to change the color of the border when it is focused
+          //     ),
+          //     hintStyle: TextStyle(
+          //       color: Colors.grey, // this is used to change the color of the hint text
+          //     ),
+          //   ),
+          //   textTheme: TextTheme(
+          //     headlineLarge: TextStyle(
+          //       //color: Colors.black, // this is used to change the color of the headline text
+          //       fontSize: 100, // this is used to change the font size of the headline text
+          //       fontWeight: FontWeight.bold, // this is used to change the font weight of the headline text
+          //     ),
+          //   ),
+          // ),// this is used to set the dark theme of the app. ThemeData.dark() is a predefined dark theme that contains all the properties of the dark theme. it is used to set the colors, fonts, and other properties of the dark theme. it is passed as a parameter to the MaterialApp constructor.
+          themeMode: ThemeMode
+              .light, // this is used to set the theme mode of the app. ThemeMode is an enum that contains three values: light, dark, and system. it is used to set the theme mode of the app. it is passed as a parameter to the MaterialApp constructor.
+          //home: ThemeTest(),
+          initialRoute:
+              '/responsivenesspackage', // this is used to set the initial route of the app. it is a string that contains the name of the route. it is passed as a parameter to the MaterialApp constructor.
+          routes: {
+            // this is used to set the routes of the app. it is a map that contains the route name as the key and the widget as the value. it is passed as a parameter to the MaterialApp constructor.
+            // '/form': (context) => FormPage(),
+            '/validation': (context) => FormValidation(),
+            //'/grid': (context) => GridV(),
+            '/theme': (context) => ThemeTest(),
+            '/home': (context) => HomePage(),
+            '/dashboard': (context) => Dashboard(),
+            '/list': (context) => Flist(),
+            '/stackw': (context) => StackW(),
+            '/stackwadv': (context) => Stackwadv(),
+            '/ownwidget': (context) => OwnWidget(),
+            '/responsiveness': (context) => Responsiveness(),
+            '/responsivenesspackage': (context) => ResponsivenessPackage(),
+          },
+        );
+      },
     );
   }
 }
---
+++ lib/responsiveness_package.dart
@@ -0,0 +1,191 @@
+import 'package:daily_practice/dp_appbar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class ResponsivenessPackage extends StatelessWidget {
+  const ResponsivenessPackage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Orientation orientation = MediaQuery.of(context).orientation; // this is used to get the orientation of the screen. it is an Orientation enum that contains two values: portrait and landscape. it is used to make the app responsive by using the orientation of the screen to set the size of the widgets. it is used to check if the orientation of the screen is portrait or landscape by using the orientation variable. it returns true if the orientation of the screen is portrait, otherwise it returns false.
+    return Scaffold(
+      appBar: DpAppbar(title: 'Responsiveness Package'),
+      body: SingleChildScrollView(
+        scrollDirection: orientation == Orientation.landscape ? Axis.horizontal : Axis.vertical,
+        child: Center(
+          child: orientation == Orientation.landscape ? Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:[
+              Container(
+                height: 200.r,
+                width: 100.r,
+                color: Colors.teal
+              ),
+
+              SizedBox(height: 20.h), // this is used to add some space between the container and the button. it is a double that contains the height of the space. it is passed as a parameter to the SizedBox constructor.
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 0.5.sw,
+                    height: 200.h,
+                    color: Colors.purple,
+                    child: Text(
+                      'My actual width ${0.5.sw} and height ${200.h}',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.h),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: 150, // this is used to set the maximum width of the container. it is a double that contains the maximum width of the container. it is passed as a parameter to the BoxConstraints constructor.
+                  maxHeight: 150, // this is used to set the maximum height of the container. it is a double that contains the maximum height of the container. it is passed as a parameter to the BoxConstraints constructor.
+                ).hw,
+                color: Colors.orange,
+                child: Text(
+                  'Hello World',
+                  style: TextStyle(
+                    fontSize: 20.sp, // this is used to set the font size of the text. it is a double that contains the font size of the text. it is passed as a parameter to the TextStyle constructor.
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              SizedBox(height: 20.h), // this is used to add some space between the container and the button. it is a double that contains the height of the space. it is passed as a parameter to the SizedBox constructor.
+              //without using flutter_screenutil package, the container will not be responsive and it will not adjust its size according to the screen size. it will have a fixed size of 200x200 pixels regardless of the screen size. it will look good on a small screen but it will look small on a large screen.
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+              ),
+        
+              SizedBox(height: 20),
+        
+              Text(
+                'This is a text without using flutter_screenutil package',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green,
+                ),
+                textAlign: TextAlign.center,
+              ),
+        
+              SizedBox(height: 20),
+        
+              //with using flutter_screenutil package, the container will be responsive and it will adjust its size according to the screen size. it will have a size of 200x200 pixels on a small screen and it will have a size of 400x400 pixels on a large screen. it will look good on both small and large screens.
+        
+              Container(
+                height: 200.h,
+                width: 200.w,
+                color: Colors.blue,
+              ),
+        
+              SizedBox(height: 20.h),
+              Text(
+                'This is a responsive text with flutter_screenutil package',
+                style: TextStyle(
+                  fontSize: 20.sp,
+                  color: Colors.green,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ) : Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children:[
+              Container(
+                height: 200.r,
+                width: 100.r,
+                color: Colors.teal
+              ),
+
+              SizedBox(height: 20.h), // this is used to add some space between the container and the button. it is a double that contains the height of the space. it is passed as a parameter to the SizedBox constructor.
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 0.5.sw,
+                    height: 200.h,
+                    color: Colors.purple,
+                    child: Text(
+                      'My actual width ${0.5.sw} and height ${200.h}',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        color: Colors.white,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 20.h),
+              Container(
+                constraints: BoxConstraints(
+                  maxWidth: 150, // this is used to set the maximum width of the container. it is a double that contains the maximum width of the container. it is passed as a parameter to the BoxConstraints constructor.
+                  maxHeight: 150, // this is used to set the maximum height of the container. it is a double that contains the maximum height of the container. it is passed as a parameter to the BoxConstraints constructor.
+                ).hw,
+                color: Colors.orange,
+                child: Text(
+                  'Hello World',
+                  style: TextStyle(
+                    fontSize: 20.sp, // this is used to set the font size of the text. it is a double that contains the font size of the text. it is passed as a parameter to the TextStyle constructor.
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              SizedBox(height: 20.h), // this is used to add some space between the container and the button. it is a double that contains the height of the space. it is passed as a parameter to the SizedBox constructor.
+              //without using flutter_screenutil package, the container will not be responsive and it will not adjust its size according to the screen size. it will have a fixed size of 200x200 pixels regardless of the screen size. it will look good on a small screen but it will look small on a large screen.
+              Container(
+                height: 200,
+                width: 200,
+                color: Colors.red,
+              ),
+        
+              SizedBox(height: 20),
+        
+              Text(
+                'This is a text without using flutter_screenutil package',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.green,
+                ),
+                textAlign: TextAlign.center,
+              ),
+        
+              SizedBox(height: 20),
+        
+              //with using flutter_screenutil package, the container will be responsive and it will adjust its size according to the screen size. it will have a size of 200x200 pixels on a small screen and it will have a size of 400x400 pixels on a large screen. it will look good on both small and large screens.
+        
+              Container(
+                height: 200.h,
+                width: 200.w,
+                color: Colors.blue,
+              ),
+        
+              SizedBox(height: 20.h),
+              Text(
+                'This is a responsive text with flutter_screenutil package',
+                style: TextStyle(
+                  fontSize: 20.sp,
+                  color: Colors.green,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
